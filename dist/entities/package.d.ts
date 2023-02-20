127e2cdca4c006487d5f5d862cbfd3ad2cca3bf9
Jonathan <jonathanelian64@gmail.com>
2023-02-19 23:00:16 -0700

    Delete dist folder

--- dist/entities/package.d.ts
+++
@@ -1,1 +0,0 @@
-export type PackageManager = 'pnpm' | 'npm' | 'yarn';
--- dist/tools/file.d.ts
+++
@@ -1,9 +0,0 @@
-import { PackageManager } from '../entities/package.js';
-export declare function copyTemplate(framework: string, lang: string, runtime: string, folderProject: string): void;
-/**
- * Executes a shell command and return it as a Promise.
- * @param cmd {string}
- * @return {Promise<string>}
- */
-export declare function execShellCommand(cmd: string): Promise<unknown>;
-export declare const installDeps: (packageManager: PackageManager, nameProject: string, isDeno: boolean) => Promise<void>;
